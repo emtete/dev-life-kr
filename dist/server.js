2f89166eeece760fcb50eff56663cdf6d4430e7f
최종호 <victor_77@naver.com>
2021-07-29 17:29:51 +0900

    .

--- dist/server.js
+++ dist/server.js
@@ -11,5 +11,4 @@ console.log('server started '+ port);
 // git add .
 // git commit -m "."
 // heroku buildpacks:set heroku/nodejs
-// git push heroku master
 // git subtree push --prefix dist heroku master
