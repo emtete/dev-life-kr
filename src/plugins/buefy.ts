6fcf8d5fa8ff9067ea5ad76bf7ad2a7bd6cc1151
최종호 <victor_77@naver.com>
2021-07-29 13:59:01 +0900

    buefy icon test

---
+++ src/plugins/buefy.ts
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+import { faFontAwesome, faGit, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// internal icons
+// import { faUserSecret, faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+//   faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+//   faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload } from "@fortawesome/free-solid-svg-icons";
+
+
+// library.add(faUserSecret, faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+//     faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+//     faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload, faFontAwesome)
+library.add(faFontAwesome, fas, faGit, faGithubSquare)
+
+Vue.component('vue-fontawesome', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas',
+});
+
+// Vue.use(Buefy, {
+//   defaultIconComponent: "vue-fontawesome",
+//   defaultIconPack: "fas",
+//   customIconPacks: {
+//     fas: {
+//       sizes: {
+//         default: "lg",
+//         "is-small": "1x",
+//         "is-medium": "2x",
+//         "is-large": "3x"
+//       },
+//       iconPrefix: ""
+//     }
+//   }
+// });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <b-button label="Launch toast (default)" size="is-medium" @click="toast" /> -->
+    <!-- <i class="fab fa-500px"></i> -->
+    <i class="fab fa-github-square"></i>
+    <i class="fab fa-git-square"></i>
+    <b-button size="is-small" icon-left="user">
+      Add
+    </b-button>
+    <b-button size="is-small" icon-pack="fab" icon-left="font-awesome">
+      Add
+    </b-button>
+
+    <b-button size="is-small" icon-pack="fab" icon-left="github-square">
+      Add
+    </b-button>
+
+    <b-icon pack="fas" icon="home" size="is-medium"> </b-icon>
+    <b-icon pack="fab" icon="fa-500px" size="is-small"> </b-icon>
+    <b-icon pack="fas" icon="user" size="is-large" type="is-success"></b-icon>
+    <!-- <b-icon pack="fa" icon="faGit" size="is-large" type="is-success"></b-icon> -->
+    <vue-fontawesome :icon="['fab', 'font-awesome']" />
+    <vue-fontawesome :icon="['fab', 'faGit']" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
@@ -19,16 +40,18 @@ export default class Home extends Vue {
   url = `https://api-dev-life-kr.herokuapp.com/post/getScrollList?userId=1&numberOfRequest=10`;
 
   created() {
-
     axios
       .get(this.url)
-      .then((result) => {
+      .then(result => {
         console.log(result);
-        
       })
-      .catch((err) => {
+      .catch(err => {
         alert(err);
       });
+  }
+
+  toast() {
+    this.$buefy.toast.open('Something happened');
   }
 }
 </script>
